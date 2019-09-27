1ebd05c9f1fedf2803d550d0d11db038110d75b6
Syed Salah Uddin Mahmood <ssu.mahmood@gmail.com>
2019-09-27 04:17:31 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,8 +2,9 @@ def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = 'It's-a me, Mario!'
   puts phrase
+    puts status
 end
 
 def toadstool
-  puts status
+
 end
